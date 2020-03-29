5991f0d97449bcf86fcf5f8aa2c77e0b828a0a11
Arthur Oliveira Viana <46693700+VianaArthur@users.noreply.github.com>
2020-03-29 14:28:34 -0300

    Assets

---
+++ assets/js/script.js
@@ -0,0 +1,66 @@
+const form = document.getElementById("form");
+form.addEventListener("submit", handleSubmit);
+
+function handleSubmit(event) {
+  event.preventDefault();
+
+  const gender = getSelectedValue("gender");
+  const age = getInputNumberValue("age");
+  const weight = getInputNumberValue("weight");
+  const height = getInputNumberValue("height");
+  const activityLevel = getSelectedValue("activity_level");
+
+  if (validateForm()) {
+    getCalorieResult(gender, age, weight, height, activityLevel);
+  }
+}
+
+function getSelectedValue(id) {
+  const select = document.getElementById(id);
+  return select.options[select.selectedIndex].value;
+}
+
+function getInputNumberValue(id) {
+  return Number(document.getElementById(id).value);
+}
+
+function validateForm() {
+  // TODO
+  return true;
+}
+
+function getCalorieResult(gender, age, weight, height, activityLevel) {
+  // Result fields do be displayed in result-container
+  const bmr = Math.round(
+    gender === "female"
+      ? 655 + 9.6 * weight + 1.8 * height - 4.7 * age
+      : 66 + 13.7 * weight + 5 * height - 6.8 * age
+  );
+
+  const maintenance = Math.round(bmr * Number(activityLevel));
+  const loseWeight = maintenance - 450;
+  const gainWeight = maintenance + 450;
+
+  const layout = `
+    <h2>Here's your result:</h2>
+    <div class="result-content">
+        <ul>
+            <li>
+            Your Basal Metabolic Rate is <strong> ${bmr} calories</strong>.
+            </li>
+            <li>
+            To maintain your weight, you need to consume on average <strong> ${maintenance} calories</strong>.
+            </li>
+            <li>
+            To loose weight, you need to consume on average<strong> ${loseWeight} calories</strong>.
+            </li>
+            <li>
+            To gain weight, you need to consume on average<strong> ${gainWeight} calories</strong>.
+            </li>
+        </ul>
+    </div>
+    `;
+
+  const result = document.getElementById("result");
+  result.innerHTML = layout;
+}
